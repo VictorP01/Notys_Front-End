a1053390ac986931881c23d68a26c61a65f6c410
Victor Paulo da Silva Arruda <victor0070000@gmail.com>
2021-08-25 23:14:19 -0300

    Connect register with the API

--- src/components/auth/register_form/index.js
+++ src/components/auth/register_form/index.js
@@ -10,6 +10,7 @@ import {
   Label,
 } from "rbx";
 import { Redirect } from "react-router-dom";
+import UserService from "../../../services/users";
 
 const RegisterForm = () => {
   const [name, setName] = useState("");
@@ -18,12 +19,26 @@ const RegisterForm = () => {
   const [redirectToLogin, setRedirectToLogin] = useState(false);
   const [error, setError] = useState(false);
 
+  const HandleSubmit = async (evt) => {
+    evt.preventDefault();
+    try {
+      const user = await UserService.register({
+        name: name,
+        email: email,
+        password: password,
+      });
+      setRedirectToLogin(true);
+    } catch (error) {
+      setError(true);
+    }
+  };
+
   if (redirectToLogin) return <Redirect to={{ pathname: "/login" }} />;
 
   return (
     <Fragment>
       <Column.Group centered>
-        <form>
+        <form onSubmit={HandleSubmit}>
           <Column size={12}>
             <Field>
               <Label size="small">Name:</Label>
