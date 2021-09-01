46bae99e95feed56d47712d579ca34c3fa92eb6e
Victor Paulo da Silva Arruda <victor0070000@gmail.com>
2021-08-31 22:46:49 -0300

    update users and styles

--- src/screens/users/edit/index.js
+++ src/screens/users/edit/index.js
@@ -1,6 +1,9 @@
 import React, { Fragment } from "react";
 import { Column, Section, Title, Container, Card, Button } from "rbx";
 import "../../../styles/users.scss";
+import UsersEditForm from "../../../components/users/userEditForm/index.js";
+import UsersEditFormPassword from "../../../components/users/userEditPasswordForm/index.js";
+import UsersDelete from "../../../components/users/userDelete/index.js";
 import HeaderLogged from "../../../components/header_logged";
 
 const UsersEditScreen = () => {
@@ -12,10 +15,12 @@ const UsersEditScreen = () => {
           <Column.Group centered className="users-edit">
             <Column size={4}>
               <Title size={5} className="has-text-grey has-text-left">
-                Informações Pessoais
+                Your personal information
               </Title>
               <Card>
-                <Card.Content>Users Edit Form...</Card.Content>
+                <Card.Content>
+                  <UsersEditForm />
+                </Card.Content>
               </Card>
             </Column>
           </Column.Group>
@@ -26,13 +31,15 @@ const UsersEditScreen = () => {
                 Password
               </Title>
               <Card>
-                <Card.Content>Users Edit Password Form...</Card.Content>
+                <Card.Content>
+                  <UsersEditFormPassword />
+                </Card.Content>
               </Card>
             </Column>
           </Column.Group>
           <Column.Group centered>
             <Column size={4} className="has-text-right">
-              Users Delete Button...
+              <UsersDelete />
             </Column>
           </Column.Group>
         </Container>
