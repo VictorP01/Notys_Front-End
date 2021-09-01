6a40c6c1794cff8d3280536e284eb58089f8a6ca
Victor Paulo da Silva Arruda <victor0070000@gmail.com>
2021-08-31 22:53:39 -0300

    Add update name in header_logged

--- src/components/header_logged/index.js
+++ src/components/header_logged/index.js
@@ -9,6 +9,7 @@ import { faList } from "@fortawesome/free-solid-svg-icons";
 
 function HeaderLogged(props) {
   const [redirectToHome, setRedirectToHome] = useState(false);
+  const [user, setUser] = useState(localStorage.getItem("user"));
 
   const logOut = async () => {
     await UserService.logout();
@@ -65,7 +66,7 @@ function HeaderLogged(props) {
             <Dropdown>
               <Dropdown.Trigger>
                 <Button className="button" color="white" outlined>
-                  <span>Victor ▼</span>
+                  <span>{JSON.parse(user)["name"]}</span>
                 </Button>
               </Dropdown.Trigger>
               <Dropdown.Menu>
